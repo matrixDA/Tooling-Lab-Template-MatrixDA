7120637cedd49c39c0b08f826b8ac37a5667944d
dinesh2003 <adhikaridinesh76@gmail.com>
2024-07-21 23:11:43 -0400

    worked on unit testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   "env": {
     "browser": true,
-    "es2021": true
+    "node" : true,
+    "es2021": true,
+    "jest" : true
   },
   "extends": `eslint:recommended`,
   "parserOptions": {
